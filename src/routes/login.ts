485bff368aeec6040c25000d5b1e01f97b7988d4
G041 <gianfrancoguizzardi@hotmail.com>
2025-11-11 21:50:41 -0300

    cambios login terrado

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,24 +1,16 @@
 import { Router } from 'express';
 import prisma from '../prisma';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import jwt, { Secret, SignOptions } from 'jsonwebtoken';
+
+const JWT_SECRET: Secret = process.env.JWT_SECRET as string;
+if (!JWT_SECRET) {
+  throw new Error('Missing JWT_SECRET env var');
+}
+
+const JWT_EXPIRES = (process.env.JWT_EXPIRES ?? "15m") as SignOptions["expiresIn"];;
 
 const router = Router();
-
-/**
- * GET /restaurants → lista todos (incluye lat/long)
- */
-router.get('/', async (_req, res) => {
-  
-  try {
-    const users = await prisma.user.findMany();
-    res.json(users);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Error fetching user list' });
-  }
-});
-
 
 // POST /login → log user in
 router.post('/', async (req, res) => {
@@ -35,24 +27,30 @@ router.post('/', async (req, res) => {
     })
 
     if (!user) {
-      return res.status(400).json({ error: 'User not found' })
+      return res.status(404).json({ error: 'User not found' })
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
     if ( !validPassword ) {
-      return res.status(400).json({ error: 'Incorrect password for this user.'})
+      return res.status(401).json({ error: 'Incorrect password for this user.'})
     } 
 
-    res.status(201).json({
-      "accessToken": "<JWT corto (p.e. 5-15 min)>",
-      "refreshToken": "<refresh token (largo, revocable)>",
-      "expiresIn": 900,         // segundos (opcional)
-      "user": { "id": 123, "username": "juan" }
+    // Genero JWT
+    const token = jwt.sign(
+      { userId: user.id_user },         // payload
+      JWT_SECRET,                  // secret
+      { expiresIn: JWT_EXPIRES }   // options (NOT the 2nd arg)
+    )
+
+    res.status(200).json({
+      accessToken: token,
+      expiresIn: JWT_EXPIRES, // minutes        
+      user: { id: user.id_user, username: user.name }
     });
 
   } catch (err) {
     console.error(err);
-    res.status(400).json({ error: 'Error trying to log in', details: err });
+    res.status(500).json({ error: 'Error trying to log in', details: err });
   }
 });
 
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,13 +1,60 @@
 import { Router } from 'express';
 import prisma from '../prisma';
+import bcrypt from 'bcrypt';
+import jwt, { Secret, SignOptions } from 'jsonwebtoken';
+
+const JWT_SECRET: Secret = process.env.JWT_SECRET as string;
+if (!JWT_SECRET) {
+  throw new Error('Missing JWT_SECRET env var');
+}
+
+const JWT_EXPIRES = (process.env.JWT_EXPIRES ?? "15m") as SignOptions["expiresIn"];;
+
+const SALT_ROUNDS = 10;
 
 const router = Router();
 
-/**
- * GET /restaurants → lista todos (incluye lat/long)
- */
-router.get('/', async (_req, res) => {
+// POST /signup → log user in
+router.post('/', async (req, res) => {
   
+  const { emailAddress, username, password } = req.body;
+  try {
+    const userByName = await prisma.user.findFirst({
+      where: {
+          name: username
+      },
+    })
+
+    const userByEmail = await prisma.user.findFirst({
+        where: {
+            email: emailAddress
+        },
+    })
+
+    if (userByName) {
+        return res.status(404).json({ error: 'Username already taken' })
+    }
+
+    if (userByEmail) {
+        return res.status(404).json({ error: 'Email already registered' })
+    }
+
+    if ( !emailAddress.includes('@') ) {
+        return res.status(404).json({ error: 'Email address is not valid' })
+    }
+
+    if ( username.length > 15 || username.length < 4 ) {
+        return res.status(404).json({ error: 'Username length is invalid' })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error trying to log in', details: err });
+  }
 });
 
 
